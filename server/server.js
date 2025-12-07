8b654af536b648ba40907ad44f27c30c57c9c698
Anand Kumar Pandit <anandkumarpandit613@gmail.com>
2025-12-07 14:42:00 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ const applicationRoutes = require('./routes/application');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || '*',
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
